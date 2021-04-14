9bbea660336b1464ac274439475de4b7bbbf455a
Muhammad shoaib <muhammadshoaib0k0@gmail.com>
2021-04-14 16:52:21 +0500

    Add project files.

---
+++ Scripts/JS/Component1.jsx
@@ -0,0 +1,23 @@
+﻿
+function Shehroz() {
+    return (
+        <div>
+            <h1>my name is murshad</h1>
+            <p>
+                this is all about to learn language .... requirement ka hissab sa banday
+                badalna taiil ki zarorrat mursad ki karam nawazi ki waja sa hum passs
+                hoty hain machine hain bhai yeh robot ki tarha kam karta hain bs .
+      </p>
+            <ul>
+                <li>java </li>
+                <li>javascript</li>
+                <li>ajax</li>
+                <li>bootstrap</li>
+                <li>jquery</li>
+            </ul>
+        </div>
+    );
+}
+//export default Shehroz;
+
+React.render(<Shehroz />, document.getElementById("com1"));
---
+++ Scripts/JS/Component2.jsx
@@ -0,0 +1,17 @@
+﻿function Name() {
+
+    const First = "ali";
+    const Last = " g ";
+    return (
+        <div>
+            <h1>
+                the name of the person is {First + " " + Last}
+            </h1>
+
+            <h2> and the again {First} {Last}</h2>
+        </div>
+    );
+}
+//export default Name;
+
+React.render(<Name />, document.getElementById("com2"));
